493cbeb0d36977d24e04246a966a22ee2812c08e
IgorCamposRatao <igorcamposratao@gmail.com>
2023-04-10 21:54:15 -0300

    to do list feito

---
+++ todolist/src/components/Task/index.jsx
@@ -0,0 +1,19 @@
+import { useRef } from 'react';
+import './style.css'
+
+export default function Task(props) {
+    const ref = useRef(`${props.key}`)
+    
+    return (
+        <div className="Task">
+            <div className="TaskDescription">
+                {props.description}
+            </div>
+            <div className="TaskActions">
+                <button className='danger' onClick={() => props.removeCallback(props.taskId)}>Remove</button>
+                <button>Edit</button>
+            </div>
+
+        </div>
+    );
+}
